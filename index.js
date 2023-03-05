f03f1f3ff9c36c68cbfa89a06ac8f2217d97edb0
Arrey-Etta <33989423+okhot@users.noreply.github.com>
2023-03-05 14:06:00 +0100

    Update fetching with axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
 const getCatsByFriendliness = require("./endpoints");
 const { config } = require('dotenv');
@@ -9,10 +10,9 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get("/", async (req, res) => {
-  await fetch("https://api.thecatapi.com/v1/breeds")
-    .then((response) => response.json())
-    .then((response) => res.send(getCatsByFriendliness(response, req.query)));
+server.get("/",  (req, res) => {
+   axios.get("https://api.thecatapi.com/v1/breeds")
+    .then((response) => res.send(getCatsByFriendliness(response.data, req.query)));
 });
 
 server.listen(process.env.PORT, () => console.log(`running port ${process.env.PORT}...`));
